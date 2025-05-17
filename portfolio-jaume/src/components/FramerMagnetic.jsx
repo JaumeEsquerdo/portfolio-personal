9d10d079be9b85389380e7c5a853ba7838656e92
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-05-17 15:48:20 +0200

    update FramerMagnetic, efecto magnetico

--- portfolio-jaume/src/components/FramerMagnetic.jsx
+++ portfolio-jaume/src/components/FramerMagnetic.jsx
@@ -1,18 +1,42 @@
+
 import { motion } from 'framer-motion';
 import { useRef, useState } from 'react';
 
 export const FramerMagnetic = ({ children }) => {
-    const ref = useRef(null)
+    /* recibe children, es decir, cualquier contenido que metas dentro de este componente. */
 
+    const ref = useRef(null)
     const [position, setPosition] = useState({ x: 0, y: 0 });
 
     const mouseMove = (e) => {
         const { clientX, clientY } = e;
+        const { width, height, left, top } = ref.current.getBoundingClientRect()
+        /*getBoundingClientRect() devuelve un objeto con las propiedades del tamaño y posición del elemento en la ventana. */
+
+        const x = clientX - (left + width / 2)
+        const y = clientY - (top + height / 2)
+        /* Obtiene las dimensiones (width, height) y posición (left, top) del div referenciado.
+        Se usa para calcular el centro del elemento.*/
+        /* Calcula la diferencia entre el cursor y el centro del elemento. Así obtienes una coordenada relativa al centro, para un "efecto imán" hacia el puntero. */
+        setPosition({ x, y })
 
     }
 
+    const mouseLeave = () => {
+        setPosition({ x: 0, y: 0 })
+        /* reseteo */
+    }
+    /* se extrae para usarlo en la anim.  */
+    const { x, y } = position
     return (
-        <motion.div ref={ref}>
+        <motion.div
+            onMouseMove={mouseMove}
+            onMouseLeave={mouseLeave}
+            ref={ref}
+            animate={{ x, y }}
+            transition={{ type: 'spring', stiffness: 150, damping: 15, mass: 0.3 }}
+
+        >
             {
                 children
             }
@@ -20,3 +44,17 @@ export const FramerMagnetic = ({ children }) => {
     );
 }
 
+/** 
+ * TRANISITION
+ * type: 'spring': usa una animación muelle que da rebote suave y realista.
+ * stiffness: qué tan rígido es el efecto muelle. Más alto = más rápido pero más brusco.
+ * damping: cuánto frena la vibración del muelle. Más alto = menos rebote.
+ * mass: qué tan pesado es el objeto. Más bajo = más ágil, más alto = más lento
+ * 
+ * Parámetros por default de la animación:
+ * 
+ *    stiffness	100	- Cuánto "tira" el resorte. Más alto = más rápido.
+ *    damping	10	- Cuánto se amortigua el movimiento. Más alto = menos rebote.
+ *    mass	1	- Peso del objeto. Más alto = más lento, más inercia.
+ *    velocity	0	- Velocidad inicial. Normalmente no lo cambias
+ */
--- portfolio-jaume/src/pages/Proyectos.jsx
+++ portfolio-jaume/src/pages/Proyectos.jsx
@@ -1,29 +1,40 @@
 import '@/css/proyectos.css'
+import { FramerMagnetic } from '@/components/FramerMagnetic';
 
 const Proyectos = () => {
     return (
-        
 
 
-            <div className="ProyectosWrapper">
-                <h1 className='Proyectos-h1'>Proyectos página</h1>
 
-                <div className='Proyectos-container'>
+        <div className="ProyectosWrapper">
+            <h1 className='Proyectos-h1'>Proyectos página</h1>
+
+            <div className='Proyectos-container'>
+
+                <FramerMagnetic>
                     <div className='Proyecto'>
                         <h2>Proyecto titulo 1</h2>
                         <button>Link al proyecto</button>
                     </div>
+                </FramerMagnetic>
+
+                <FramerMagnetic>
+
                     <div className='Proyecto'>
                         <h2>Proyecto titulo 2</h2>
                         <button>Link al proyecto</button>
                     </div>
+                </FramerMagnetic>
+
+                <FramerMagnetic>
                     <div className='Proyecto'>
                         <h2>Proyecto titulo 3</h2>
                         <button>Link al proyecto</button>
                     </div>
-                </div>
+                </FramerMagnetic>
             </div>
-       
+        </div>
+
     );
 }
 
