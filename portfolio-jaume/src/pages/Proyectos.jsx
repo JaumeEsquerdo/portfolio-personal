8d173365a52df44b9eaeeebf7c82845f0e436e1b
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-08-28 14:58:23 +0200

    Update Proyectos.jsx

--- portfolio-jaume/src/pages/Proyectos.jsx
+++ portfolio-jaume/src/pages/Proyectos.jsx
@@ -8,12 +8,15 @@ const Proyectos = () => {
         {
             id: 1,
             numero: "01",
-            titulo: "App de Emergencias",
-            descripcion: "Aplicación informativa sobre desastres y emergencias.",
-            imagen: "/img/proyecto1.jpg",
-            tecnologias: ["React", "CSS", "API REST"],
-            tipo: "frontend y backend"
-        },
+            titulo: "Modernidad Mediterránea – Revista",
+            descripcion:
+                "Página estilo blog interactivo para inspirar el uso del diseño moderno mediterráneo, creada como proyecto final del bootcamp.",
+            tecnologias: ["HTML", "CSS", "JavaScript (vanilla)"],
+            metodologia: "SUITCSS (CSS modular)", // campo opcional
+            stack: "Frontend",
+            imagen: "/img/modernidad-mediterranea.png",
+            enlace: "https://github.com/JaumeEsquerdo/Modernidad-Mediterranea---revista",
+        }, ,
         {
             id: 2,
             numero: "02",
@@ -38,10 +41,10 @@ const Proyectos = () => {
     return (
         <section className="ProyectosWrapper">
             <header className='Proyectos-header'>
-           
-            <h2 className='Proyectos-h2'>Mis proyectos</h2>
-            <h3 className='Proyectos-h3'>Estos proyectos han sido diseñados y desarrollados por mí, en algunos casos utilizando imágenes o fragmentos de contenido como referencia, pero siempre creando el layout, la estructura y, cuando ha sido necesario, también el backend desde cero.</h3>
-             </header>
+
+                <h2 className='Proyectos-h2'>Mis proyectos</h2>
+                <h3 className='Proyectos-h3'>Estos proyectos han sido diseñados y desarrollados por mí, en algunos casos utilizando imágenes o fragmentos de contenido como referencia, pero siempre creando el layout, la estructura y, cuando ha sido necesario, también el backend desde cero.</h3>
+            </header>
 
             <div className='Proyectos-container'>
 
