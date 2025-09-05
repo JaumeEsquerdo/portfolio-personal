e98add531d16e7c33a547f10ecee0b4db8ee69d8
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-09-05 18:27:29 +0200

    Update Contacto.jsx

--- portfolio-jaume/src/pages/Contacto.jsx
+++ portfolio-jaume/src/pages/Contacto.jsx
@@ -13,9 +13,12 @@ const Contacto = () => {
 
             <div className="Contacto-block">
                 <h2 className="Contacto-title">Enlaces</h2>
-                <a className='Contacto-link' href="https://github.com/JaumeEsquerdo" target="_blank">GitHub</a>
-                <a className='Contacto-link' href="https://www.linkedin.com/in/jaume-esquerdo/" target="_blank">LinkedIn</a>
-                <a className='Contacto-link' href="/CV-jaume.pdf" target="_blank">Ver CV</a>
+                <a className='Contacto-link' href="https://github.com/JaumeEsquerdo" target="_blank" rel="noopener noreferrer"
+                >GitHub</a>
+                <a className='Contacto-link' href="https://www.linkedin.com/in/jaume-esquerdo/" target="_blank" rel="noopener noreferrer"
+                >LinkedIn</a>
+                <a className='Contacto-link' href="/CV-jaume.pdf" target="_blank" rel="noopener noreferrer"
+                >Ver CV</a>
             </div>
         </main>
 
