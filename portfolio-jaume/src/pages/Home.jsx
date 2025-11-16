905df08a84e41b37d927555438c5ab39ed02cb30
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-11-16 21:20:04 +0100

    Update Home.jsx

--- portfolio-jaume/src/pages/Home.jsx
+++ portfolio-jaume/src/pages/Home.jsx
@@ -46,7 +46,7 @@ export const Home = () => {
       <div className="PortfolioPresentation">
         <h1 className="PortfolioPresentation-h1">JAUME ESQUERDO RAMIS</h1>
         <h3 className="PortfolioPresentation-h3">
-          Desarrollador Fullstack en formación
+          Desarrollador Frontend en formación
         </h3>
         <h3 className="PortfolioPresentation-h3">
           Actualmente he realizado el máster de ‘Bootcamp de Desarrollo Web, UX
