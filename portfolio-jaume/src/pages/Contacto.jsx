4a10e7d42abf57f994c0f8f042c77c50eafefe10
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-11-26 18:37:16 +0100

    Update Contacto.jsx

--- portfolio-jaume/src/pages/Contacto.jsx
+++ portfolio-jaume/src/pages/Contacto.jsx
@@ -1,28 +1,50 @@
-import '@/css/contacto.css'
+import "@/css/contacto.css";
 
 const Contacto = () => {
-    return (<>
+  return (
+    <>
+      <main className="Contacto">
+        <div className="Contacto-block">
+          <h2 className="Contacto-title">Contacto</h2>
+          <a className="Contacto-link" href="mailto:jaume.esquerdo@hotmail.com">
+            jaume.esquerdo@hotmail.com
+          </a>
+          <a className="Contacto-link" href="tel:+34676097535">
+            +34 676 09 75 35
+          </a>
+          <p className="Contacto-p">Villajoyosa, Alicante</p>
+        </div>
 
-        <main className="Contacto">
-            <div className="Contacto-block">
-                <h2 className="Contacto-title">Contacto</h2>
-                <a className='Contacto-link' href="mailto:jaume.esquerdo@hotmail.com">jaume.esquerdo@hotmail.com</a>
-                <a className='Contacto-link' href="tel:+34676097535">+34 676 09 75 35</a>
-                <p className='Contacto-p'>Villajoyosa, Alicante</p>
-            </div>
-
-            <div className="Contacto-block">
-                <h2 className="Contacto-title">Enlaces</h2>
-                <a className='Contacto-link' href="https://github.com/JaumeEsquerdo" target="_blank" rel="noopener noreferrer"
-                >GitHub</a>
-                <a className='Contacto-link' href="https://www.linkedin.com/in/jaume-esquerdo/" target="_blank" rel="noopener noreferrer"
-                >LinkedIn</a>
-                <a className='Contacto-link' href="/CV-jaume.pdf" target="_blank" rel="noopener noreferrer"
-                >Ver CV</a>
-            </div>
-        </main>
-
-    </>);
-}
+        <div className="Contacto-block">
+          <h2 className="Contacto-title">Enlaces</h2>
+          <a
+            className="Contacto-link"
+            href="https://github.com/JaumeEsquerdo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          <a
+            className="Contacto-link"
+            href="https://www.linkedin.com/in/jaume-esquerdo/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            className="Contacto-link"
+            href="/CV-jaume-frontend.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Ver CV
+          </a>
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default Contacto;
