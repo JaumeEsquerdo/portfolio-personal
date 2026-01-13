3777b0360c52ca5319caa3f1a8531466435e72e2
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-01-13 11:25:38 +0100

    update transicion de entrada de contenido en proyectos.jsx

--- portfolio-jaume/src/pages/Proyectos.jsx
+++ portfolio-jaume/src/pages/Proyectos.jsx
@@ -1,38 +1,71 @@
-import '@/css/proyectos.css'
-import Proyecto from '@/components/Proyecto';
-import listaProyectos from '@/data/listaProyectos';
+import "@/css/proyectos.css";
+import Proyecto from "@/components/Proyecto";
+import listaProyectos from "@/data/listaProyectos";
+import { motion } from "framer-motion";
 
+const container = {
+  hidden: {}, //inicial
+  show: {
+    transition: {
+      staggerChildren: 0.2, //escalado de 0.2s
+    },
+  },
+};
+
+const item = {
+  hidden: { opacity: 0, y: 18 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 2.8,
+      ease: [0.22, 1, 0.36, 1],
+    },
+  },
+};
+/* margin: '-100px' para q ejecute la transición un poco antes del viewport
+y once:true, para q se ejecute solo una vez
+*/
 const Proyectos = () => {
+  return (
+    <motion.section
+      className="ProyectosWrapper"
+      variants={container}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: true, margin: "-100px" }}
+    >
+      <motion.header variants={container} className="Proyectos-header">
+        <motion.h2 className="Proyectos-h2" variants={item}>
+          Proyectos
+        </motion.h2>
+        <motion.h3 className="Proyectos-h3" variants={item}>
+          Estos proyectos han sido diseñados y desarrollados por mí, en algunos
+          casos utilizando imágenes o fragmentos de contenido como referencia,
+          pero siempre creando el layout, la estructura y, cuando ha sido
+          necesario, también el backend desde cero.
+        </motion.h3>
+      </motion.header>
 
-    return (
-        <section className="ProyectosWrapper">
-            <header className='Proyectos-header'>
-
-                <h2 className='Proyectos-h2'>Proyectos</h2>
-                <h3 className='Proyectos-h3'>Estos proyectos han sido diseñados y desarrollados por mí, en algunos casos utilizando imágenes o fragmentos de contenido como referencia, pero siempre creando el layout, la estructura y, cuando ha sido necesario, también el backend desde cero.</h3>
-            </header>
-
-            <div className='Proyectos-container'>
-
-                {listaProyectos.map((proyecto) => (
-                    <Proyecto
-                        key={proyecto.id}
-                        numero={proyecto.numero}
-                        titulo={proyecto.titulo}
-                        descripcion={proyecto.descripcion}
-                        imagen={proyecto.imagen}
-                        stack={proyecto.stack}
-                        herramientas={proyecto.herramientas}
-                        enlace={proyecto.enlace}
-                        destacado={proyecto.destacado}
-                        deploy={proyecto.deploy}
-                    />
-                ))}
-
-            </div>
-        </section>
-
-    );
-}
+      <div className="Proyectos-container" variants={container}>
+        {listaProyectos.map((proyecto) => (
+          <motion.div key={proyecto.id} variants={item}>
+            <Proyecto
+              numero={proyecto.numero}
+              titulo={proyecto.titulo}
+              descripcion={proyecto.descripcion}
+              imagen={proyecto.imagen}
+              stack={proyecto.stack}
+              herramientas={proyecto.herramientas}
+              enlace={proyecto.enlace}
+              destacado={proyecto.destacado}
+              deploy={proyecto.deploy}
+            />
+          </motion.div>
+        ))}
+      </div>
+    </motion.section>
+  );
+};
 
 export default Proyectos;
