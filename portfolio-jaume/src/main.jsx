04faf505bb1fe43952938a2a5019fe8f7d405b54
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-04-16 19:47:03 +0200

    Update main.jsx

--- portfolio-jaume/src/main.jsx
+++ portfolio-jaume/src/main.jsx
@@ -1,14 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import CustomCursor from "./components/CustomCursor";
 import router from "../lib/routes/router";
 import "@/css/index.css";
 import "@/css/home.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CustomCursor />
     <RouterProvider router={router} />
   </StrictMode>,
 );
